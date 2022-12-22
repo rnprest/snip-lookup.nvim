1d2ec5abc39f6bf32515437639c8ca8b3952e7d8
Ryan Preston <r.preston73@gmail.com>
2022-12-22 12:17:40 -0500

    feat: remove windows from build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-cargo build --release
-
-cp target/release/libsnip_lookup.dylib lua/snip_lookup.so
-cp lua/snip_lookup.so lua/snip_lookup.dll
+cargo build --all --release && mv target/release/libsnip_lookup.dylib lua/snip_lookup.so
