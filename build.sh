85c7223446493ea41e6fbcf0d4ea35b7c86d739e
Ryan Preston <r.preston73@gmail.com>
2023-01-23 08:39:21 -0500

    cleanup: remove 2 unused files

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cargo build --all --release && mv target/release/libsnip_lookup.dylib lua/snip_lookup.so
--- tag_new_release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-current_tag=$(cargo get version --pretty)
-
-git tag "$current_tag"
-git push origin "$current_tag"
